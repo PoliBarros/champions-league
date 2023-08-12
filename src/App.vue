42814065ba8d034be2aca5bd0a7ae3e8e1c4648e
Poliana Barros <pbarros@screamingpower.com>
2023-08-12 13:53:24 -0400

    data handling

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,90 @@
 <template>
   <div id="app">
-    <SkeletonLoader />
+    <!-- NICE TO HAVE - Skeleton Loader -->
+    <SkeletonLoader v-if="isLoading" />
+
+    <!-- Error Message -->
+    <div v-else class="club-content">
+      <div v-if="error" class="error-message">
+        An error occurred: {{ error.message }}
+      </div>
+
+      <!-- Main content -->
+      <div v-else class="club-header">
+        <h2>English Premier League</h2>
+        <button class="loadMore" v-if="showBtn" v-on:click="loadMore">
+          Load More
+        </button>
+      </div>
+
+      {{ displayList }}
+    </div>
   </div>
 </template>
 
 <script>
 import SkeletonLoader from "./components/SkeletonLoader.vue";
+import mockClubInfo from "./utils/mock";
 import "./assets/global.css";
 
 export default {
   name: "App",
   components: {
     SkeletonLoader,
+  },
+  data() {
+    return {
+      list: [],
+      displayList: [],
+      isLoading: true,
+      showBtn: false,
+      error: "",
+    };
+  },
+  mounted() {
+    //This setTimeout is used just to simulate a delay to show the skeleton loading - Real code should not have this
+    setTimeout(() => {
+      fetch(
+        "https://www.thesportsdb.com/api/v1/json/3/lookuptable.php?l=4328&s=2020-2021"
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          if (data && data.table) {
+            // concat the fetched data (table) with mocked data (tooltip)
+            this.list = data.table.map((item) => {
+              const matchedIdTooltip = mockClubInfo.find(
+                (mock) => mock.idStanding === item.idStanding
+              );
+              return { ...item, ...matchedIdTooltip };
+            });
+            this.showBtn = true;
+
+            //NICE TO HAVE - show the first 5 items when load
+            const itemsToAdd = this.list.splice(0, 5);
+            this.displayList = this.displayList.concat(itemsToAdd);
+          }
+          this.isLoading = false;
+        })
+        .catch((error) => {
+          this.error = error;
+          this.isLoading = false;
+          console.error("Fetch error:", error);
+        });
+    }, 1000);
+  },
+  methods: {
+    loadMore() {
+      //NICE TO HAVE - Load 3 more items when button clicked
+      if (this.list.length === 0) {
+        this.showBtn = false;
+        return;
+      }
+      const elementsToAdd = this.list.splice(0, 3);
+      this.displayList = this.displayList.concat(elementsToAdd);
+
+      //If the list is empty, hide the button
+      this.showBtn = this.list.length === 0 ? false : true;
+    },
   },
 };
 </script>
@@ -27,5 +100,43 @@ export default {
   align-items: center;
   justify-content: center;
   overflow: auto;
+}
+.club-content {
+  height: 100%;
+  width: 90%;
+}
+.club-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border: 5px solid transparent;
+  margin-top: 50px;
+}
+.loadMore {
+  padding: 10px;
+  cursor: pointer;
+  color: var(--font-color);
+  background-color: #fff;
+  font-weight: bold;
+  border: 2px solid var(--font-color);
+  transition: all 0.3s ease-in;
+  box-shadow: var(--shadow);
+  text-transform: uppercase;
+}
+.loadMore:hover {
+  transform: scale(1.1);
+}
+
+@media (max-width: 414px) {
+  h2 {
+    font-size: 1.1rem;
+  }
+  .club-header {
+    flex-direction: column;
+    gap: 5px;
+  }
+  .loadMore:hover {
+    transform: scale(1);
+  }
 }
 </style>
---
+++ src/utils/mock.js
@@ -0,0 +1,112 @@
+/* Mock data for Tooltip text
+    idStanding : to identify the item
+    tooltip: text to be used on tooltip
+*/
+ const mockClubInfo = [
+        {
+            idStanding: "322876",
+            tooltip: `Manchester City Football Club are an English professional football club based 
+            in Manchester that competes in the Premier League, the top flight of English football. 
+            Founded in 1880 as St. Mark's, they became Ardwick Association Football Club in 1887 and Manchester City in 1894.`},
+        {
+            idStanding: "322877",
+            tooltip: `Manchester United Football Club, commonly referred to as Man United, or simply United, is a professional football club based 
+            in Old Trafford, Greater Manchester, England. 
+            The club competes in the Premier League, the top division in the English football league system.`},
+            
+        {
+            idStanding: "322878",
+            tooltip: `Liverpool Football Club is a professional football club based in Liverpool, England. The club competes in the Premier League, the top tier of English football. 
+            Founded in 1892, the club joined the Football League the following 
+            year and has played its home games at Anfield since its formation.`},
+        
+        {
+            idStanding: "322879",
+            tooltip: `Chelsea Football Club is an English professional football club based in Fulham, 
+            West London. Founded in 1905, they play their home games at Stamford Bridge. 
+            The club competes in the Premier League, the top division of English football. 
+            They won their first major honour, the League championship, in 1955.`},
+        
+        {
+            idStanding: "322880",
+            tooltip: `Leicester City Football Club is a professional association football club based in Leicester, in the East Midlands of England. 
+            They play their home matches at the King Power Stadium and compete in the EFL Championship, the second level of the English football league system.`},
+        
+        {
+            idStanding: "322881",
+            tooltip: `West Ham United Football Club is an English professional football club that plays its 
+            home matches in Stratford, East London. The club competes in the Premier League, the top tier of English football. 
+            The club plays at the London Stadium, having moved from their former home, the Boleyn Ground, in 2016.`},
+        
+        {
+            idStanding: "322882",
+            tooltip: `Tottenham Hotspur Football Club, commonly referred to as simply Tottenham or Spurs, is a professional football club based in 
+            Tottenham, London, England. It competes in the Premier League, the top flight of English football.`},
+        
+        {
+            idStanding: "322883",
+            tooltip: `Arsenal Football Club is an English professional football club based in Holloway, 
+            North London. Arsenal play in the Premier League, the top flight of English football.`},
+        
+        {
+            idStanding: "322884",
+            tooltip: `Leeds United Football Club is a professional football club based in Leeds, West Yorkshire in England. 
+            The club play their home matches at Elland Road and compete in the Championship,
+            the second tier of the English football league system. `},
+        
+        {
+            idStanding: "322885",
+            tooltip: `Everton Football Club is an English professional association football club based in Liverpool 
+            that competes in the Premier League, the top tier of English football.`},
+        
+        {
+            idStanding: "322886",
+            tooltip: `Aston Villa Football Club, commonly referred to as Villa, is a professional football club based in Aston, Birmingham, England. 
+            The club competes in the Premier League, the top tier of the English football league system. 
+            Founded in 1874, they have played at their home ground, Villa Park, since 1897.`},
+        
+        {
+            idStanding: "322887",
+            tooltip: `Newcastle United Football Club, commonly referred to as Newcastle, 
+            is an English professional football club based in Newcastle upon Tyne, Tyne and Wear. The club compete in the Premier League.`},
+        
+        {
+            idStanding: "322888",
+            tooltip: `Wolverhampton Wanderers Football Club, commonly known as Wolves, is a professional football club based in Wolverhampton, 
+            England, which competes in the Premier League. The club has played at Molineux Stadium since moving from Dudley Road in 1889.`},
+        
+        {
+            idStanding: "322889",
+            tooltip: `Crystal Palace Football Club, commonly referred to as Palace, is a professional football club based in Selhurst in the Borough of Croydon,
+            South London, England, which competes in the Premier League, the highest level of English football.`},
+        
+        {
+            idStanding: "322890",
+            tooltip: `Southampton Football Club is an English professional football club based in Southampton, Hampshire, that compete in the EFL Championship, 
+            the second tier of English football. Their home ground since 2001 has been St Mary's Stadium, 
+            before which they were based at The Dell. The club play in red and white shirts. `},
+        {
+            idStanding: "322891",
+            tooltip: `Brighton & Hove Albion Football Club, commonly referred to simply as Brighton, is an English professional 
+            football club based in the city of Brighton and Hove. They compete in the Premier League, the top tier of the English football league system. `},
+        {
+            idStanding: "322892",
+            tooltip: `Burnley Football Club is an English association football club based in Burnley, Lancashire, 
+            that competes in the Premier League, the first tier of English football`},
+        {
+            idStanding: "322893",
+            tooltip: `Fulham Football Club is a professional football club based in Fulham, London, England. 
+            The team competes in the Premier League, the top level of the English football league system.`},
+        {
+            idStanding: "322894",
+            tooltip: `West Bromwich Albion Football Club is a professional football club based in West Bromwich, West Midlands, England. 
+            It competes in the EFL Championship, the second tier of English football. 
+            The club was formed in 1878 and has played at its home ground, The Hawthorns, since 1900. `},
+        {
+            idStanding: "322895",
+            tooltip: `Sheffield United Football Club is a professional football club in Sheffield, South Yorkshire, England, that competes in the Premier League, 
+            following their promotion from the 2022–23 EFL Championship. They are nicknamed "the Blades" due to Sheffield's history of cutlery production.`}
+]
+        
+
+export default mockClubInfo;
